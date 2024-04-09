80fc6bf9bd65c88dc01084e18872e44d77906a6e
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-10 02:25:56 +0300

    Styles search result section

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -9,9 +9,21 @@ const SearchItem = () => {
             <span className="siDistance">500m from center</span>
             <span className="siTaxiOp">Free airport taxi</span>
             <span className="siSubtitle">Studio Apartment with Air conditioning</span>
-            <span className='siFeatures'></span>
+            <span className='siFeatures'>Entire studio • 1 bathroom • 21m² bed</span>
+            <span className="siCancelOp">Free cancellation</span>
+            <span className="siCancelOpSubtitle">You can cancel later, so lock in this great price today!</span>
         </div>
-        <div className="siDetails"></div>
+        <div className="siDetails">
+            <div className="siRating">
+                <span>Excellent</span>
+                <button>8.9</button>
+            </div>
+            <div className="siDetailTexts">
+                <span className='siPrice'>$123</span>
+                <span className='siTaxOp'>Includes taxes and fees</span>
+                <button className="siCheckButton">See availability</button>
+            </div>
+        </div>
     </div>
   )
 }
